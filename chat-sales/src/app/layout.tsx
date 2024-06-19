f66d3591a2af40eaa0c7ee6636a071c52a3d99c8
luis antonio <tyluis20@gmail.com>
2024-06-19 16:34:41 -0300

    fix project folders

--- chat-sales/src/app/layout.tsx
+++ chat-sales/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
+import { dark } from "@clerk/themes";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     // apenas para mvp modificar para uma autenticação melhor na versão pro....
-    <ClerkProvider>
+    <ClerkProvider appearance={{ baseTheme: dark }}>
       <html lang="en">
         <body className={inter.className}>{children}</body>
       </html>
